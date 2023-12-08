ed898c5e836fb5d3243e8f4d51b6db5d0f24ee70
Lasse Schier Christiansen <lasse.10@hotmail.dk>
2023-12-08 13:13:01 +0100

    fix: wrong dependencies

--- login-app/features/step-definitions/userLoginStep.js
+++ login-app/features/step-definitions/userLoginStep.js
@@ -1,4 +1,4 @@
-const { Given, When, Then } = require('cucumber');
+const { Given, When, Then } = require('@cucumber/cucumber');
 const { Builder, By, until } = require('selenium-webdriver');
 
 let driver;
